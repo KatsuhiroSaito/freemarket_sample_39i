d6e07927ae45e2c860b4efdb23247e6c056957be
KatsuhiroSaito <katuhiro_sub-106@yahoo.co.jp>
2019-03-29 22:42:10 +0900

    mouseenter/mouseleaveからhoverに変更

--- app/assets/javascripts/channels/header.js
+++ app/assets/javascripts/channels/header.js
@@ -2,28 +2,31 @@ $(function(){
 
 
   // 「カテゴリーから探す」にホバーしたときに、大カテゴリー一覧を表示する。
-  $('.header-find__list').mouseenter(function(){
-    $(this).find('.header-find-box-parent').css('display', 'block');
-  });
-  $('.header-find__list').mouseleave(function(){
-    $('.header-find-box-parent').css('display', 'none');
+  $('.header-find__list').hover(
+    function(){
+      $(this).find('.header-find-box-parent').css('display', 'block');
+  },
+    function() {
+      $('.header-find-box-parent').css('display', 'none');
   });
 
 
   // // 大カテゴリーの1つにホバーしたとき、中カテゴリーの一覧を表示する。
-  $('.header-find-box-parent__list').mouseenter(function(){
-    $(this).find('.header-find-box-child').css('display', 'block');
-  });
-  $('.header-find-box-parent__list').mouseleave(function() {
-    $('.header-find-box-child').css('display', 'none');
+  $('.header-find-box-parent__list').hover(
+    function(){
+      $(this).find('.header-find-box-child').css('display', 'block');
+  },
+    function() {
+      $('.header-find-box-child').css('display', 'none');
   });
 
 
   // 中カテゴリーの1つにホバーしたとき、小カテゴリーの一覧を表示する。
-  $('.header-find-box-child__list').mouseenter(function(){
-    $(this).find('.header-find-box-grandchild').css('display', 'block');
-  });
-  $('.header-find-box-child__list').mouseleave(function(){
-    $(this).find('.header-find-box-grandchild').css('display', 'none');
+  $('.header-find-box-child__list').hover(
+    function(){
+      $(this).find('.header-find-box-grandchild').css('display', 'block');
+  },
+    function() {
+      $('.header-find-box-grandchild').css('display', 'none');
   });
 });
