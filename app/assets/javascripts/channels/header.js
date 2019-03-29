a3b35332d703f09fa94f229de16ce6897ffe4f4d
KatsuhiroSaito <katuhiro_sub-106@yahoo.co.jp>
2019-03-29 21:23:52 +0900

    ホバーに合わせてカテゴリー一覧やブランド一覧が表示されるようにheader.jsを編集

--- app/assets/javascripts/channels/header.js
+++ app/assets/javascripts/channels/header.js
@@ -1,9 +1,29 @@
 $(function(){
+
+
+  // 「カテゴリーから探す」にホバーしたときに、大カテゴリー一覧を表示する。
   $('.header-find__list').mouseenter(function(){
-    $('.header-find-box__parent').css('display', 'block');
-    console.log('hello');
+    $(this).find('.header-find-box-parent').css('display', 'block');
   });
   $('.header-find__list').mouseleave(function(){
-    $('.header-find-box__parent').css('display', 'none');
+    $('.header-find-box-parent').css('display', 'none');
+  });
+
+
+  // // 大カテゴリーの1つにホバーしたとき、中カテゴリーの一覧を表示する。
+  $('.header-find-box-parent__list').mouseenter(function(){
+    $(this).find('.header-find-box-child').css('display', 'block');
+  });
+  $('.header-find-box-parent__list').mouseleave(function() {
+    $('.header-find-box-child').css('display', 'none');
+  });
+
+
+  // 中カテゴリーの1つにホバーしたとき、小カテゴリーの一覧を表示する。
+  $('.header-find-box-child__list').mouseenter(function(){
+    $(this).find('.header-find-box-grandchild').css('display', 'block');
+  });
+  $('.header-find-box-child__list').mouseleave(function(){
+    $(this).find('.header-find-box-grandchild').css('display', 'none');
   });
 });
