44751ca1667c165285f218a2c9216b5e802d6250
KatsuhiroSaito <katuhiro_sub-106@yahoo.co.jp>
2019-03-31 11:39:29 +0900

    header.jsを編集

--- app/assets/javascripts/channels/header.js
+++ app/assets/javascripts/channels/header.js
@@ -1,32 +1,25 @@
 $(function(){
 
 
-  // 「カテゴリーから探す」にホバーしたときに、大カテゴリー一覧を表示する。
-  $('.header-find__list').hover(
-    function(){
-      $(this).find('.header-find-box-parent').css('display', 'block');
-  },
-    function() {
-      $('.header-find-box-parent').css('display', 'none');
-  });
-
-
-  // // 大カテゴリーの1つにホバーしたとき、中カテゴリーの一覧を表示する。
-  $('.header-find-box-parent__list').hover(
-    function(){
-      $(this).find('.header-find-box-child').css('display', 'block');
-  },
-    function() {
-      $('.header-find-box-child').css('display', 'none');
-  });
-
-
-  // 中カテゴリーの1つにホバーしたとき、小カテゴリーの一覧を表示する。
+  // 中カテゴリーの1つにホバーしたとき、その親の大カテゴリーに'active-parent'というクラス名を与える。
   $('.header-find-box-child__list').hover(
     function(){
-      $(this).find('.header-find-box-grandchild').css('display', 'block');
-  },
+      $(this).parent('ul.header-find-box-child').parent('li.header-find-box-parent__list').addClass("active-parent");
+    },
     function() {
-      $('.header-find-box-grandchild').css('display', 'none');
-  });
+      $(this).parent('ul.header-find-box-child').parent('li.header-find-box-parent__list').removeClass("active-parent");
+    }
+  );
+
+
+  // 小カテゴリーの1つにホバーしたとき、その親の中カテゴリーに'active-child'というクラス名を与える。
+  $('.header-find-box-grandchild__list').hover(
+    function(){
+      $(this).parent('ul.header-find-box-grandchild').parent('li.header-find-box-child__list').addClass("active-child");
+      console.log("hello");
+    },
+    function() {
+      $(this).parent('ul.header-find-box-grandchild').parent('li.header-find-box-child__list').removeClass("active-child");
+    }
+  );
 });
