4964bfc2790066bb3353ff50f6829618d5df2ee9
hujiyuta <hujiyuta@gmail.com>
2019-04-07 17:43:05 +0900

    Edited_for_issues_at_routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'samples#sample'
-  resources  :samples do
-    collection do
-      get 'login'
-      get 'signup'
-      get 'creditCards'
-      get 'sellingItems'
-    end
-  end
+  # resources  :samples do
+  #   collection do
+  #     get 'login'
+  #     get 'signup'
+  #     get 'creditCards'
+  #     get 'sellingItems'
+  #   end
+  # end
 end
