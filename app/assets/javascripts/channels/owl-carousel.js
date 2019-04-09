7c98fb2637156327881d57335ee6a6cbfa78967d
KatsuhiroSaito <katuhiro_sub-106@yahoo.co.jp>
2019-04-09 15:51:40 +0900

    スライドが切り替わるタイミングと速度を調整

--- app/assets/javascripts/channels/owl-carousel.js
+++ app/assets/javascripts/channels/owl-carousel.js
@@ -4,8 +4,9 @@ $(function(){
     nav:true,
     items:1,
     autoplay:true,
-    autoplayTimeout:5300,
-    autoplayHoverPause:true
+    autoplayTimeout:5000,
+    autoplayHoverPause:true,
+    smartSpeed: 1000
   });
   // owl-dotにホバーしたら表示が切り替わる
   $('.slide-banner .owl-dot').hover(function() {
