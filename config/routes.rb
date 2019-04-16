b4687f8a4fc56057492e97c143f8ff09242e648d
KatsuhiroSaito <katuhiro_sub-106@yahoo.co.jp>
2019-04-16 14:02:24 +0900

    ログイン、新規登録、マイページのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'samples#sample'
   get "/confirm" => "samples#confirm"
   root 'samples#index'
-  resources  :users, only: [:index, :new]
+  resources :users, only: [:index, :new] do
+    resource :mypages do
+      collection do
+        get 'card'
+      end
+    end
+  end
 end
