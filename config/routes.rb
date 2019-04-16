756b8055cea73c43605a9430bc5f88d601dead5e
KatsuhiroSaito <katuhiro_sub-106@yahoo.co.jp>
2019-04-16 14:06:30 +0900

    ルートページのルーティングをproducts#index（トップページ）に変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'samples#sample'
+  root 'products#index'
   get "/confirm" => "samples#confirm"
-  root 'samples#index'
   resources :users, only: [:index, :new] do
     resource :mypages do
       collection do
