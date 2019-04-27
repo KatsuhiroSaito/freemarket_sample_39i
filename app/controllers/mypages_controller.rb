e33832511b12c366e8184fb5ee55163f746e1655
KatsuhiroSaito <katuhiro_sub-106@yahoo.co.jp>
2019-04-27 09:27:11 +0900

    パンくずリストが表示されるようにmypages_controllerとviewファイルを編集

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -1,15 +1,29 @@
 class MypagesController < ApplicationController
+  @user_mypage = Rails.root
+  add_breadcrumb 'メルカリ', '/'
+  before_action :add_mypage_breadcrumb
+
   def show
   end
 
   def edit
+    add_breadcrumb 'プロフィール'
+    # binding.pry
   end
 
   def card
+    add_breadcrumb '支払い方法'
     render "credit-card"
   end
 
   def logout
+    add_breadcrumb 'ログアウト'
     render "logout"
+  end
+
+
+  private
+  def add_mypage_breadcrumb
+      add_breadcrumb "マイページ", view_context.user_mypages_path(1)
   end
 end
