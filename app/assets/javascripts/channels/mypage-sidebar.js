1359856da25966ee365613de3e14f5cbd8536842
KatsuhiroSaito <katuhiro_sub-106@yahoo.co.jp>
2019-04-28 21:42:08 +0900

    微修正

--- app/assets/javascripts/channels/mypage-sidebar.js
+++ app/assets/javascripts/channels/mypage-sidebar.js
@@ -2,7 +2,7 @@ $(function(){
   var link = $('.mypage-sidebar-list__link');
   var currentUrl = location.pathname;
 
-  $(link).each(function() {
+  link.each(function() {
     if ($(this).attr("href") == currentUrl) {
       $(this).addClass("js-mypage-sidebar-current-page");
     }
